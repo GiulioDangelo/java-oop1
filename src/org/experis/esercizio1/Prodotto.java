b27001571cc1c9246fa807712c7694a8ba17aba2
GiulioDangelo <dangelogiulio48@gmail.com>
2024-03-29 14:53:00 +0100

    push

--- src/org/experis/esercizio1/Prodotto.java
+++ src/org/experis/esercizio1/Prodotto.java
@@ -1,2 +1,34 @@
-package org.experis.esercizio1;public class Prodotto {
+package org.experis.esercizio1;
+import java.util.Random;
+
+public class Prodotto {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private int iva = 22;
+
+
+    Random rand = new Random();
+    Prodotto(String name,String description, double price) {
+        id = rand.nextInt(100000000);
+
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+
+    double getBasePrice() {
+        return price;
+    }
+
+    double getFullPrice() {
+        double totalPrice = price + (price * ((double) iva / 100));
+        return Math.round(totalPrice * 100.0) / 100.0;
+    }
+
+    String getFullName() {
+       return String.format("%08d", id) + "-" + name;
+    }
 }
